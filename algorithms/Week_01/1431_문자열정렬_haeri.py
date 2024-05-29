ff1c9d23c1dc544b90a0ab91f6f608993790a5ec
haerihaeri <goflll@naver.com>
2024-05-29 20:54:23 +0900

    add 11652

--- algorithms/Week_01/1431_문자열정렬_haeri.py
+++ algorithms/Week_01/1431_문자열정렬_haeri.py
@@ -1,4 +1,4 @@
-# 데스크톱
+
 import sys
 sys.stdin=open("./Desktop/input.txt","r")
 input=sys.stdin.readline
@@ -8,7 +8,7 @@ data = []
 for _ in range(N):
     data.append(input().strip())
 
-def get_sum_of_int(x):
+def get_sum_of_int(x): # 문자열에서 숫자인 부분만 합을 계산하는 함수
     x = list(x)
     sum_of_int = 0
     for data in x:
@@ -18,7 +18,7 @@ def get_sum_of_int(x):
             pass
     return sum_of_int
 
-data = sorted(data, key=lambda x:(len(x), get_sum_of_int(x), x))
+data = sorted(data, key=lambda x:(len(x), get_sum_of_int(x), x)) # (단어의 길이, 정수의 합, 사전순정렬)
 
 for x in data:
     print(x)
